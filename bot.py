70ad55e79622aecfebc11935613cd7e4dfbc3597
Pranav2335 <106325687+Pranav2335@users.noreply.github.com>
2023-01-16 23:00:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQCZjHaI5owWW9XSa-4ihcMIFq-wOsSSXtoFaTTJHX0qYKPHiIsAbgOkrtCogeGws6gfAiu1KrwYgY1Yd9WOynFWGYJJ7RliTKYnhe0mKTeREUUGbmoXtCxeqLflVZ4ALXhndmnvUJdG2DoIsxQg1_RhwkPNPRXisXBW3V0euY8YK0tWTBr4MxONqFdRaWiD8e8uMTxKHsiXo4dZ53HHLWqnkkIz_qZQl6UwJYGC4HIIk6BYOxt7XFEtYl1_wn2aWNdaj5vuiNM4oN828_lO_bvBt5Oti-FyuIWEuX8g_eR0oHQfonC1Feb6jXc7v886gRs45oxBXZ00CnZMWYzpLpDnAAAAAVVV7xkA")
 
 
 
